a8f1363c21e2a0c921ff8da83ffd42b008b90654
Frans de Graaff <frans@uniqodo.com>
2018-10-01 22:31:57 +0100

    Use ssh config for tunnels

--- tunnel.sh
+++ tunnel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if ! ps aux | grep 'ssh -NfR 9000:localhost:9000 uniqodo-dev' | grep -v grep; then
-    ssh -NfR 9000:localhost:9000 uniqodo-dev
+if ! ps aux | grep 'ssh -Nf tun.xdebug' | grep -v grep; then
+    ssh -Nf tun.xdebug
 fi
 
-if ! ps aux | grep 'ssh -NfL 3306:localhost:3306 uniqodo-dev' | grep -v grep; then
-    ssh -NfL 3306:localhost:3306 uniqodo-dev
+if ! ps aux | grep 'ssh -Nf tun.mysql' | grep -v grep; then
+    ssh -Nf tun.mysql
 fi
